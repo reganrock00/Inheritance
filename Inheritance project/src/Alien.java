a3a4ec01d5d75c037063be17167953645cd7dd1d
CyberPower <CyberPower@10.0.0.48>
2017-12-21 00:22:36 -0700

    close to finish

--- Inheritance project/src/Alien.java
+++ Inheritance project/src/Alien.java
@@ -1,5 +1,20 @@
 
-public class Alien
+public abstract class Alien extends SuperHero
 	{
-
+		public void notWelcomed()
+		{
+			System.out.println("Go back to where you came from");
+		}
+		public void speakWeirdLanguage()
+		{
+			System.out.println("lrukfh ldfh lkhjuhglkjnlgjnb dfjvh");
+		}
+		public abstract void seekJustice();
+		{
+			System.out.println("They will pay for their deeds");
+		}
+		public abstract void useSuperAbility();
+		{
+			System.out.println("BOOM!");
+		}
 	}
--- Inheritance project/src/ExtraTerrestrial.java
+++ Inheritance project/src/ExtraTerrestrial.java
@@ -1,5 +1,12 @@
 
-public class ExtraTerrestrial
+public abstract class ExtraTerrestrial extends Alien
 	{
-
+		public void lookWeird()
+		{
+			System.out.println("So what I've got tentacles");
+		}
+		public void abductHuman()
+		{
+			System.out.println("We need them for...science...stuff");
+		}
 	}
